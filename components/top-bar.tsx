de1082fb2ae1dd5b887c2b85934609c8e54717ab
Sebastian <sebastian.varelagi@gmail.com>
2025-01-06 01:56:53 -0500

    Update top-bar.tsx
    Se actualiza el color

--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -4,8 +4,8 @@ import { Mail, MapPin } from "lucide-react";
 export const TopBar = () => {
 
   return (
-    <div className="w-full bg-gray-800">
-      <div className="hidden sm:flex mx-auto justify-evenly items-center w-[90%] h-16 gap-x-4 text-white">
+    <div className="w-full bg-primary">
+      <div className="hidden sm:flex mx-auto justify-evenly items-center w-[90%] h-12 gap-x-4 text-white">
         <div className="flex flex-row space-x-4">
           <MapPin/>
           <p>
