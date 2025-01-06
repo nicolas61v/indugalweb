84386b6d0ca068d21fe0065907c9a539cbd368de
Sebastian <sebastian.varelagi@gmail.com>
2025-01-06 01:57:28 -0500

    Update footer.tsx
    Cambio de color y bold

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,13 @@ import { FacebookIcon, InstagramIcon, LinkedinIcon, XIcon } from './icons'
 
 export const Footer = () => {
   return (
-    <footer className="w-full h-full">
-      <div className="w-full h-full p-12 flex flex-col justify-center items-start gap-y-10 px-8">
-        <div className='space-y-6 p-4'>
+    <footer className="w-full h-full bg-black">
+      <div className="flex flex-col items-start justify-center w-full h-full p-12 px-8 gap-y-10">
+        <div className='p-4 space-y-6'>
           <div className='text-2xl font-bold'>
             <h1>GalvaGroup</h1>
           </div>
-          <p className='text-md text-gray-400'>Galvanizados Industriales del Aburrá SAS - Industrias y Galvanizados SAS</p>
+          <p className='text-gray-400 text-md'>Galvanizados Industriales del Aburrá SAS - Industrias y Galvanizados SAS</p>
           <div className='space-x-4'>
           <Link
 						isExternal
@@ -42,10 +42,10 @@ export const Footer = () => {
 					</Link>
           </div>
         </div>
-        <div className='flex flex-col gap-y-6 md:flex-row justify-start gap-x-10 w-full p-4'>
+        <div className='flex flex-col justify-start w-full p-4 gap-y-6 md:flex-row gap-x-10'>
           <div className='w-1/4'>
             <ul className='space-y-6 text-gray-400'>
-              <p className='text-white font-bold'>Enlances Útiles</p>
+              <p className='font-bold text-white'>Enlances Útiles</p>
               <li>1</li>
               <li>2</li>
               <li>3</li>
@@ -54,7 +54,7 @@ export const Footer = () => {
           </div>
           <div className='w-1/4'>
             <ul className='space-y-6 text-gray-400'>
-              <p className='text-white font-bold'>Nuestras Empresas</p>
+              <p className='font-bold text-white'>Nuestras Empresas</p>
               <li>1</li>
               <li>2</li>
               <li>3</li>
@@ -63,7 +63,7 @@ export const Footer = () => {
           </div>
           <div className='w-1/4'>
             <ul className='space-y-6 text-gray-400'>
-              <p className='text-white font-bold'>Redes</p>
+              <p className='font-bold text-white'>Redes</p>
               <li>1</li>
               <li>2</li>
               <li>3</li>
