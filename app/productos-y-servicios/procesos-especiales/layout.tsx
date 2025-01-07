4bb648209f07c0d2f6f2a72931e1fe95f309cbcc
nicolas61v <vasquezjuannicolas71@gmail.com>
2025-01-06 21:03:35 -0500

    added  new special procees

--- app/productos-y-servicios/procesos-especiales/layout.tsx
+++ app/productos-y-servicios/procesos-especiales/layout.tsx
@@ -1,13 +1,12 @@
-export default function AboutLayout({
+// app/procesos-especiales/layout.tsx
+export default function ProcesosEspecialesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        {children}
-      </div>
-    </section>
+    <div className="min-h-screen bg-gray-50 pb-32">
+      {children}
+    </div>
   );
 }
